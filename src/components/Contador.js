8e437f4cb8275b507fc71709af3313561587318b
adrianpg10 <adrianperezgomezsmr@gmail.com>
2022-05-09 19:26:31 +0200

    Contador Clic

---
+++ src/components/Contador.js
@@ -0,0 +1,14 @@
+
+import "../estilos/Contador.css";
+
+function Contador({ numClics }) {
+
+    return (
+
+        <div className="contador">
+            {numClics}
+        </div>
+    );
+}
+
+export default Contador;
